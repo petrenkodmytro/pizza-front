d8aee00b0005ab821b3cb7417e822bd4376d9cc8
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2024-01-19 21:26:48 +0200

    fix size image

--- src/modules/menu/components/menu-item.tsx
+++ src/modules/menu/components/menu-item.tsx
@@ -8,14 +8,11 @@ interface MenuItemProps {
   title: string;
   ingredients?: string | null;
   price: number;
-  fitImage?: boolean
+  fitImage?: boolean;
 }
 
-const MenuItem: FC<MenuItemProps> = ({ image, weight, title, ingredients, price, fitImage }) => {
-  const transfomations = ['w_384', 'h_240'];
-  if (fitImage) {
-    transfomations.unshift('c_pad');
-  }
+const MenuItem: FC<MenuItemProps> = ({ image, weight, title, ingredients, price }) => {
+  const transfomations = ["c_auto", "w_384", "h_240"];
 
   const imageCld = useCloudinaryImage(image, transfomations);
 
@@ -23,7 +20,9 @@ const MenuItem: FC<MenuItemProps> = ({ image, weight, title, ingredients, price,
     <div className="w-72 lg:w-96 shadow-xl rounded-2xl bg-white flex flex-col">
       <div className="relative">
         <AdvancedImage cldImg={imageCld} width={384} height={240} className="rounded-2xl" />
-        <span className="absolute bottom-1.5 right-3 bg-gray-900/50 text-white text-sm px-2 rounded-[2rem]">{weight} g</span>
+        <span className="absolute bottom-1.5 right-3 bg-gray-900/50 text-white text-sm px-2 rounded-[2rem]">
+          {weight} g
+        </span>
       </div>
       <div className="p-4 sm:p-8 flex flex-col justify-between h-[calc(100%_-_15rem)] flex-1">
         <div>
@@ -33,7 +32,7 @@ const MenuItem: FC<MenuItemProps> = ({ image, weight, title, ingredients, price,
         </div>
         <div className="flex justify-between items-center flex-col gap-3 sm:flex-row">
           <span className="text-xl font-semibold">{price} грн.</span>
-          <button>Додати до корзини</button>
+          <button>Кошик</button>
         </div>
       </div>
     </div>
